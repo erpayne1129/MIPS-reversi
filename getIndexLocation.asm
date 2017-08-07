1a6572f7e81cc9cba4bc31e7defbee4ce02873f3
Geronimo213 <lopez.geronimo213@gmail.com>
2017-08-07 09:39:21 -0500

    Fixed formula for translating (x,y) to array

--- getIndexLocation.asm
+++ getIndexLocation.asm
@@ -1,5 +1,6 @@
 .text
 getIndexLocation: #Take an x,y coordinate and translate it into a single-array index location
-mulu $t0, $a0, 8
-add $t0, $t0, $a1
-move $a0, $t0
+mulu $t0, $a1, 8
+add $t0, $t0, $a0
+move $v0, $t0
+jr $ra
