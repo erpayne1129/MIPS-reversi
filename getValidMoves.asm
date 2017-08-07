974e6e09731f6abbd0b48e701cf19dac47f3caff
Geronimo213 <lopez.geronimo213@gmail.com>
2017-08-07 09:28:40 -0500

    Implemented $s0-$s7 register saving to stack

--- getValidMoves.asm
+++ getValidMoves.asm
@@ -3,6 +3,18 @@ validMoves: .word 0:64
 .text
 
 getValidMoves:
+#Store contents of $s0-$s7 to stack
+addi $sp, $sp, -32
+sw $s0, ($sp)
+sw $s1, 4($sp)
+sw $s2, 8($sp)
+sw $s3, 12($sp)
+sw $s3, 16($sp)
+sw $s4, 20($sp)
+sw $s5, 24($sp)
+sw $s6, 28($sp)
+sw $s7, 32($sp)
+
 move $s0, $a0
 move $s1, $a1
 move $s2, $ra
@@ -60,3 +72,16 @@ sb $t7 ($t6)
 #Restore return address and set output validMoves root
 move $ra, $s2
 move $v0, $t0
+#Restore contents of $s0-$s7
+lw $s0, ($sp)
+lw $s1, 4($sp)
+lw $s2, 8($sp)
+lw $s3, 12($sp)
+lw $s3, 16($sp)
+lw $s4, 20($sp)
+lw $s5, 24($sp)
+lw $s6, 28($sp)
+lw $s7, 32($sp)
+addi $sp, $sp, 32
+
+jr $ra
--- isValidMove-1.asm
+++ isValidMove-1.asm
@@ -1,5 +1,17 @@
 .text
 isValidMove:
+#Save entire contents of $s0-$s7
+addi $sp, $sp, -32
+sw $s0, ($sp)
+sw $s1, 4($sp)
+sw $s2, 8($sp)
+sw $s3, 12($sp)
+sw $s3, 16($sp)
+sw $s4, 20($sp)
+sw $s5, 24($sp)
+sw $s6, 28($sp)
+sw $s7, 32($sp)
+
 #Move arguments to temporaries (so that  original arguments are not overwritten)
 move $s0, $a0
 move $s1, $a1
@@ -34,5 +46,16 @@ li $s5, 0x4f #set other tile to O
 flippingTiles:
 
 isInvalidMove: #move is invalid, so output false and jump return
+#Restore contents of $s0-$s7
+lw $s0, ($sp)
+lw $s1, 4($sp)
+lw $s2, 8($sp)
+lw $s3, 12($sp)
+lw $s3, 16($sp)
+lw $s4, 20($sp)
+lw $s5, 24($sp)
+lw $s6, 28($sp)
+lw $s7, 32($sp)
+addi $sp, $sp, 32
 li $v0, 0
 jr $ra
