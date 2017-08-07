d4a5026aa70a38cb21a23aaee3f4bc87010af0a9
Jorge Macias <jmacias9564@gmail.com>
2017-08-07 17:22:32 -0500

    Update tilesToFlip.asm

--- tilesToFlip.asm
+++ tilesToFlip.asm
@@ -1,7 +1,7 @@
 .data
-	tilesToFlip: .space 40
-	xdirection: .word 0
-	ydirection: .word 0
+	tilesToFlip: .space 32
+	xdirection: .word 0, 1, 1, 1, 0, -1, -1, -1
+	ydirection: .word 1, 1, 0, 1, -1, -1, 0, 1
 	x: .word 4
 	y: .word 4
 	xstart: .word 0
@@ -9,6 +9,7 @@
 	isOnBoard: .word 8
 	board: .space 4, 4
 	otherTile: .asciiz 0
+	emptyspace: .asciiz " "
 
 .text
 
@@ -22,12 +23,19 @@ forXYdirection:	# line 59
 	la $t2, ydirection($t2)
 	 
 	ifisOnBoard: #line 63
-		beq $s0, $s5, true #line 63
-		bne $s0, $s5, exit1 #line 67
+		la $a0, x
+		la $a1, y
+		jal getIndexLocation #line 63 : get player location
+		lb $t7, $v0
+		beq $t7, 1, andifboard 
+		bne $t7, 1, exit1 #line 67
+		andifboard:
+		beq $t7, $s0, true
 		true:	la $t1, xdirection($t1)
 			la $t2, ydirection($t2)
 			j exit1
 		exit1: #line 68
+		
 	beq $s5, $s0, while #line 69: board, otherTile
 	bne $s5, $s0, exit2
 	while:	#line 69
@@ -37,33 +45,56 @@ forXYdirection:	# line 59
 		bne $s5, $s0, exit2
 	exit2:	#line 73
 	ifboardXY: #line 76
-		beq $s0, $s4, whileT
-		bne $s0, $s4, exit3
+		beq $s0, $s1, whileT
+		bne $s0, $s1, exit3
+		
 	whileT: #line 78
-		sw $a2, xdirection #line 79
-		sw $a3, ydirection
-		la $t1, $t1, $a2 #line 79
-		la $t2, $t2, $a3
-		bgt $s0, -1, whileT
-		ble $s0, -1, exit3
+		sw $t5, xdirection #line 79
+		sw $t6, ydirection
+		sub $t1, $t1, $t5 #line 79 : x -= xdirection
+		sub $t2, $t2, $t6 #y -= ydirection
 			ifxstart: #line 81
-			la $t3, xstart
-			beq $t1, $t3, ifystart
+			la $s2, xstart
+			beq $t1, $t5, ifystart #if x == xstart
 			ifystart: #line 81
-			la $t4, ystart
-			beq $t2, $t4, exit3
-		exit3:
+			la $s3, ystart
+			beq $t2, $s3, tilesToFlipx #if y == ystart
+				#append tilesToFlip array x, y
+				tilesToFlipx:
+				sw $a0, x #set x for getIndex func
+				sw $t1, tilesToFlip #store x into Flip array x
+				tilesToFlipy:
+				sw $a1, y #set y for getIndex func
+				li $t6, 4 #set temp reg to move array y
+				sw $t2, tilesToFlip($t6) #store y into Flip array y
+				jal getIndexLocation
+		
+		bgt $t1, -1, whileT #loop back until -1 x
+		bgt $t2, -1, whileT #loop back until -1 y
+
+		exit3: #end of while loop
+		jal tilesToFlip
+		
 		#help with line 83-88
-			
-		j isInvalidMove #used to return false : line 87
+		sw $t9, emptyspace #load emptyspace
+		la $a0, $t9 #store emptyspace into board array 0
+		la $a0, 4($t9) #store emptyspace into board array 1
+		#sw $s2, $s0 #line 85 : store xstart into board array 0
+		#sw $s3, 4($s0) #store ystart into board array 1
+		
+		beqz $v0, isInvalidMove 		
+		#j isInvalidMove #used to return false : line 87
+		move $v0, $ra
+		jr $t8
 exit:
 
+#t7 is xy coordinate
 #x, y in t1, t2
 #jump to a0 and a1 for isOnBoard
 #board in s0
 #xstart in s2
 #ystart in s3
-#playersTile s4 = tile
+#playersTile s1 = tile
 #otherTile s5
 #output tilesToFlip to v0
 
