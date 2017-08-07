77beeef8706be793857597d536cf01a9f9e5ec3a
erpayne1129 <erpayne1129@gmail.com>
2017-08-07 12:43:43 -0500

    Changed indexing scheme for board
    Changed indexing calculation from 8x+2 to 8y+2, cleaned up some bloat

--- drawBoard.asm
+++ drawBoard.asm
@@ -2,7 +2,7 @@
 #CS3340.0U1 Project: Reversi
 	.data
 
-newBoard: .byte	' ':64	#creates an empty board of 64 characters (to be used and modified)
+exampleBoard: .byte	' ':64	#creates an empty board of 64 characters (to be used and modified)
 BOARDTODRAW: .byte 'E':64	#creates an empty board of 64 characters (to be used and modified)
 HEADER:	.asciiz "  | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 |\n"	#label for each column
 HLINE:	.asciiz "  +---+---+---+---+---+---+---+---+\n"	#horizontal line for board
@@ -12,7 +12,7 @@ COLSEP:	.asciiz "| "	#column seperator for formatting
 COLSEP2:.asciiz "|\n"	#column seperator for formatting
 	.text
 DRAWBOARD:
-	la $a0, newBoard
+	la $a0, exampleBoard			#load example board (comment out in final version)
 	la $t0, BOARDTODRAW
 	# Print out the board received as argument ($a0)
 	
@@ -30,8 +30,7 @@ MOVEBOARD:
 	
 	addi $t7, $t7, 1			# Increment loop counter
 		
-	move $t9, $zero
-	addi $t9, $t9, 64
+	li $t9, 64
 	bne $t7, $t9, MOVEBOARD			#loop until counter = 64
 	
 	subi $t0, $t0, 64			#reset $t0 to be the first index of the board
@@ -56,8 +55,7 @@ FORROW:
 	syscall
 	
 	# Print the number for the current row (loop counter + 1)
-	move $t2, $t1
-	addi $t2, $t2, 1
+	addi $t2, $t1, 1			#add 1 to value of y
 	li $v0, 1 				#code 1 is to print an integer
 	la $a0, ($t2)				#print the row number
 	syscall
@@ -71,15 +69,14 @@ FORROW:
 	# Create counter for inner loop (x)
 	move $t3, $zero
 FORCOLUMN:
-	# Calculate board index ((x*8)+y)
-	# Store (x*8) in $t2
-	move $t4, $t3
-	move $t5, $zero
-	addi $t5, $t5, 8			
-	mult $t4, $t5				#multiply x by 8
+	# Calculate board index ((y*8)+x)
+	# Store (y*8) in $t2
+	move $t4, $t1				#store y into $t4
+	li $t5, 8			
+	mult $t4, $t5				#multiply y by 8
 	mflo $t4
-	add $t4, $t4, $t1			#add y to x*8
-	# Stored (x*8)+y in $t4
+	add $t4, $t4, $t1			#add x to y*8
+	# Stored (y*8)+x in $t4
 		
 	# Print column separator line
 	li $v0, 4 				#code 4 is to print a null-terminated string
@@ -87,7 +84,7 @@ FORCOLUMN:
 	syscall
 	
 	# Print correct board index (board is in $t0)
-	# Noo need to multiply index by 4 because we are using a byte array
+	# No need to multiply index by 4 because we are using a byte array
 	# Add calculated offset (in $t4) to value of $t0 and store in $t5
 	add $t5, $t4, $t0
 	li $v0, 11 				#code 4 is to print a single character
@@ -103,8 +100,7 @@ FORCOLUMN:
 	addi $t3, $t3, 1
 	
 	# Loop back to FORCOLUMN if not done 8 times yet
-	move $t6, $zero
-	addi $t6, $t6, 8
+	li $t6, 8
 	bne $t3, $t6, FORCOLUMN
 	
 	# Now back in the outer loop (FORROW)
@@ -128,8 +124,7 @@ FORCOLUMN:
 	addi $t1, $t1, 1
 	
 	# Loop back to FORROW if not done 8 times yet
-	move $t6, $zero
-	addi $t6, $t6, 8
+	li $t6, 8
 	bne $t1, $t6, FORROW	
 	
 	# If finished drawing board, return to calling address
